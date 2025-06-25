6cb5d5c247bb4984f3712dc9f3d16d53f71a64eb
Clarke Lamothe <clarkelamothe@yahoo.fr>
2025-06-25 17:20:16 -0300

    Enhance password validation by adding special character check and updating password length requirements

--- app/src/main/java/com/clarkelamothe/notemark/feature_auth/domain/UserDataValidator.kt
+++ app/src/main/java/com/clarkelamothe/notemark/feature_auth/domain/UserDataValidator.kt
@@ -3,7 +3,8 @@ package com.clarkelamothe.notemark.feature_auth.domain
 class UserDataValidator(
     private val patternValidator: PatternValidator
 ) {
-    fun isValidUsername(username: String) = username.trim().length >= MIN_USERNAME_LENGTH
+    fun isValidUsername(username: String) =
+        username.trim().length in MIN_USERNAME_LENGTH..MAX_USERNAME_LENGTH
 
     fun isValidEmail(email: String) = patternValidator.matches(email.trim())
 
@@ -12,17 +13,19 @@ class UserDataValidator(
         val hasDigit = password.any { it.isDigit() }
         val hasLowerCaseCharacter = password.any { it.isLowerCase() }
         val hasUpperCaseCharacter = password.any { it.isUpperCase() }
-
+        val hasSpecialCharacter = password.any { !it.isLetterOrDigit() }
         return PasswordValidationState(
             hasMinLength = hasMinLength,
             hasNumber = hasDigit,
             hasLowerCaseCharacter = hasLowerCaseCharacter,
-            hasUpperCaseCharacter = hasUpperCaseCharacter
+            hasUpperCaseCharacter = hasUpperCaseCharacter,
+            hasSpecialCharacter = hasSpecialCharacter
         )
     }
 
     companion object {
-        const val MIN_PASSWORD_LENGTH = 5
+        const val MIN_PASSWORD_LENGTH = 8
         const val MIN_USERNAME_LENGTH = 4
+        const val MAX_USERNAME_LENGTH = 20
     }
 }
