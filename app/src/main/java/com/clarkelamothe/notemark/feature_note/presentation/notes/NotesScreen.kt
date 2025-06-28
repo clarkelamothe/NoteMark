22a9a179d964360e4f142171a73f58d9daeb7058
Clarke Lamothe <clarkelamothe@yahoo.fr>
2025-06-28 04:01:20 -0300

    Add empty state handling to NotesScreen and update NotesState

--- app/src/main/java/com/clarkelamothe/notemark/feature_note/presentation/notes/NotesScreen.kt
+++ app/src/main/java/com/clarkelamothe/notemark/feature_note/presentation/notes/NotesScreen.kt
@@ -5,10 +5,12 @@ package com.clarkelamothe.notemark.feature_note.presentation.notes
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +20,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -80,38 +83,52 @@ fun NotesScreen(
 
         }
     ) {
-        when (orientation) {
-            Orientation.PHONE_PORTRAIT, Orientation.TABLET_PORTRAIT -> NotesScreenPortrait(
-                modifier = Modifier.padding(top = it.calculateTopPadding()),
-                onClickNote = {
-                    onAction(NotesAction.OnClickNote)
-                },
-                onLongClickNote = {
-                    onAction(NotesAction.OnLongClickNote)
-                }
-            )
+        when (state.showEmptyState) {
+            true -> {
+                Text(
+                    modifier = Modifier
+                        .padding(top = it.calculateTopPadding() + 80.dp)
+                        .fillMaxWidth(),
+                    text = "You’ve got an empty board,\n" + "let’s place your first note on it!",
+                    style = MaterialTheme.typography.titleSmall,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant,
+                    textAlign = TextAlign.Center
+                )
+            }
 
-            Orientation.PHONE_LANDSCAPE, Orientation.TABLET_LANDSCAPE -> {
-                LazyVerticalStaggeredGrid(
+            false -> when (orientation) {
+                Orientation.PHONE_PORTRAIT, Orientation.TABLET_PORTRAIT -> NotesScreenPortrait(
                     modifier = Modifier.padding(top = it.calculateTopPadding()),
-                    contentPadding = PaddingValues(16.dp),
-                    verticalItemSpacing = 16.dp,
-                    horizontalArrangement = Arrangement.spacedBy(16.dp),
-                    columns = StaggeredGridCells.Fixed(3)
-                ) {
-                    (1..10).map { nm ->
-                        item {
-                            Text(
-                                text = nm.toString(),
-                                modifier = Modifier
-                                    .background(Color.LightGray)
-                            )
+                    onClickNote = {
+                        onAction(NotesAction.OnClickNote)
+                    },
+                    onLongClickNote = {
+                        onAction(NotesAction.OnLongClickNote)
+                    }
+                )
+
+                Orientation.PHONE_LANDSCAPE, Orientation.TABLET_LANDSCAPE -> {
+                    LazyVerticalStaggeredGrid(
+                        modifier = Modifier.padding(top = it.calculateTopPadding()),
+                        contentPadding = PaddingValues(16.dp),
+                        verticalItemSpacing = 16.dp,
+                        horizontalArrangement = Arrangement.spacedBy(16.dp),
+                        columns = StaggeredGridCells.Fixed(3)
+                    ) {
+                        (1..10).map { nm ->
+                            item {
+                                Text(
+                                    text = nm.toString(),
+                                    modifier = Modifier
+                                        .background(Color.LightGray)
+                                )
+                            }
                         }
                     }
                 }
-            }
 
-            Orientation.DESKTOP, null -> TODO()
+                Orientation.DESKTOP, null -> TODO()
+            }
         }
     }
 }
--- app/src/main/java/com/clarkelamothe/notemark/feature_note/presentation/notes/NotesState.kt
+++ app/src/main/java/com/clarkelamothe/notemark/feature_note/presentation/notes/NotesState.kt
@@ -2,5 +2,6 @@ package com.clarkelamothe.notemark.feature_note.presentation.notes
 
 data class NotesState(
     val username: String = "",
-    val notes: List<Any> = emptyList()
+    val notes: List<Any> = emptyList(),
+    val showEmptyState: Boolean = false
 )
